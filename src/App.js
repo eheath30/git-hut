c0479cb325ff363558f0f17fc54b5142ea56569a
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-02 18:50:20 +0100

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Header, Footer } from "./layouts/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <Router>
+      <Header />
+        
+      <Footer />
+    </Router>
   );
 }
 
