c50287ec7c4ab931dfd696c90f0f024ef2b7e6fd
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-02 18:50:20 +0100

    header and footer

---
+++ src/layouts/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <div className="navigation">
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className="container">Header</div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
