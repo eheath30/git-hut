8e47e2c1c00e6d3cf9a398dce347b2175e20387e
eheath30 <elliot.heath@btinternet.com>
2022-08-03 10:36:37 +0100

    updating prototypes

--- src/components/searchform/SearchForm.js
+++ src/components/searchform/SearchForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SearchForm() {
     return (
-        <form className='mt-3 row'>
+        <form className='mt-3 row w-75 mx-auto'>
             <div className="row">
                 <label for="s_repo" class="col-sm-2 col-form-label">Search Repo:</label>
                 <div className="col-sm-10">
