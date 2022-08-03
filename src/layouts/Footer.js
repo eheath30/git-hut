327f74b71a7c52cb84a7c7377ffa4ef8177a33b2
eheath30 <elliot.heath@btinternet.com>
2022-08-03 18:35:28 +0100

    commiting

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
     return (
         <div className="footer">
-            <footer className="py-4 foot-colour fixed-bottom">
+            <footer className="py-2 foot-colour fixed-bottom">
                 <div className="container">
                     <p className="m-0 text-center text-white">
-                        FOOTER
+                    <i class="fa-solid fa-code-commit footerLogo"></i>
                     </p>
                 </div>
             </footer>
