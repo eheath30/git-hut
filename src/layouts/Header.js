30e5ab29180f3b5e3a590196cef0f05509cc9f89
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-03 10:05:18 +0100

    Styles

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
     return (
         <div className="">
-            <header className="py-4 text-center navbar-expand navbar-dark bg-dark">
+            <header className="py-4 text-center head-colour navbar-expand navbar-dark bg-dark">
                 <p className="text-white">HEADER</p>
             </header>
         </div>
