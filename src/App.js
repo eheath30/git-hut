14312f65cc6e2f231062ded3a9cfde9905d420b2
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-03 09:41:27 +0100

    Initial Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Header, Footer } from "./layouts/index";
+import MainPage from "./pages/MainPage/MainPage"
 
 function App() {
   return (
     <Router>
       <Header />
-        
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
       <Footer />
     </Router>
   );
