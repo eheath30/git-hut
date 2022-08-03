3188072087fc61c6a5fa2d6d5ccd12b8ae97da3e
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-03 10:05:18 +0100

    Styles

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div className="footer">
-            <footer className="py-4 bg-dark fixed-bottom">
+            <footer className="py-4 foot-colour fixed-bottom">
                 <div className="container">
                     <p className="m-0 text-center text-white">
                         FOOTER
