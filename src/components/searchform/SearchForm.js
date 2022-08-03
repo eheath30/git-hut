4b4ee0c2beadca5ed27191a5e6ecef4b20c7d099
eheath30 <elliot.heath@btinternet.com>
2022-08-03 23:21:32 +0100

    updated search

--- src/components/searchform/SearchForm.js
+++ src/components/searchform/SearchForm.js
@@ -21,8 +21,12 @@ export default function SearchForm() {
         <form className='mt-3 row w-75 mx-auto' role="form" aria-label="form" onSubmit={handleSubmit}>
             <div className="row">
                 <label htmlFor="s_repo" className="col-sm-3 col-form-label-lg"><i class="fa-solid fa-magnifying-glass"></i> Search User:</label>
-                <div className="col-sm-9 input-group-lg my-auto">
+                <div className="col-sm-7 input-group-lg my-auto">
                     <input type="text" className="form-control" id="s_repo" placeholder='Github username' onChange={handleSearchTerm} />
+
+                </div>
+                <div className="col-sm-1 align-self-end">
+                <button className="btn btn-dark folderIcon my-2"><i class="fa-solid fa-magnifying-glass-arrow-right"></i></button>
                 </div>
             </div>
         </form>
