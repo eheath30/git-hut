7c42cd85bab4fe13090068ea2e41f11ea020675e
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-03 09:58:35 +0100

    MainPage Layout

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -7,7 +7,42 @@ export default function MainPage() {
       <div className='row'>
         <Form />
       </div>
-      
+
+      <div className='container mt-3'>
+        <div class="row row-cols-1 ">
+          <div class="col mt-3">
+            <div class="card">
+              <div class="card-body">
+                This is some text within a card body 1.
+              </div>
+            </div>
+          </div>
+          <div class="col mt-3">
+            <div class="card">
+              <div class="card-body">
+                This is some text within a card body 2.
+              </div>
+            </div>
+          </div>
+          <div class="col mt-3">
+            <div class="card">
+              <div class="card-body">
+                This is some text within a card body 3.
+              </div>
+            </div>
+          </div>
+          <div class="col mt-3">
+            <div class="card">
+              <div class="card-body">
+                This is some text within a card body 4.
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
 
     </div>
   )
