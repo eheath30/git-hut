958f65a2be3cec83f57d3030d78dfc3806b6dca0
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-04 14:45:00 +0100

    History feature

--- src/pages/HistoryPage/historyPage.js
+++ src/pages/HistoryPage/historyPage.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from "react";
+
+import { UserContext } from "../../UserContext"
 
 export default function AboutPage() {
+
+  const [user, setUser] = useContext(UserContext);
+  const [historyUser, setHistoryUser] = useState([]);
+
+  useEffect(() => {
+    setHistoryUser([...historyUser, user])
+  }, [user])
+
+  console.log(historyUser)
+
   return (
     <section className='container justify-content-center mx-auto'>
       <h1 className='display-3 text-center my-4'>HISTORY PAGE</h1>
